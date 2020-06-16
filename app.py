2b33583d45ef0e8873f131697b9e6f04ff33a829
mark-soro <napoleon_soro@rocketmail.com>
2020-06-16 13:52:40 -0400

    imported request object from flask, to use the request.form attribute

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+# render_template -> when provided with the name...
+# of a template and any required arguments returns a string of html.
+# request -> provides us access to posted data from our html form
+# it has a dict attribute "form" and just like any old python dict
+# it supports bracket notation so we'll get the forms data like this...
+# request.form['phrases'] and request.form['letters'].
+
 from vsearch import search_for_letters as sfl
 
 
@@ -13,12 +20,15 @@ def hello() -> str:
     # The @app.route accepts this as 2nd argument
     # this matches our POST method in the entry.html form section.
 def do_search() -> str:
-    return str(sfl('alice in wonderland'))
+    phrase = request.form['phrase'] # using the request.form to access...
+    letters = request.form['letters'] # the form data.
+    return str(sfl(phrase, letters)) # call the sfl method on phrase and letters.
+    # return str(sfl('alice in wonderland'))->removing the hardcoded string
 
 @app.route('/entry') # this creates a new URL to the webapp
 def entry_page() -> 'html':
     return render_template('entry.html',
             the_title='Welcome to search for letters website!')
-            # ^provides a value to associate with 'the_title' argument
+            # ^provides a value to associate with 'the_title' argument.
 app.run(debug=True) # debug True enables flask to restart the webapp every
                     # time it detects a change.
