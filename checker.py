8bf93c87b8ff65dab8829f33666e3fe9c5e096fd
mark-soro <napoleon_soro@rocketmail.com>
2020-06-22 11:53:04 -0400

    re-doing the checkey.py step by step

--- checker.py
+++ checker.py
@@ -1,11 +1,11 @@
-from flask import session
-
-from functools import wraps
-
+# create a decorator module in 3 steps
+# 1) a decorator is just a function
 def check_logged_in(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if 'logged_in' in session:
-            return func(*args, **kwargs):
-        return 'You are not logged in.'
-    return wrapper        
+    # 2) define a decorated function that accepts another function as...
+    #    an argument by referring as an function object( without the "()" ).
+    def wrapper():
+        # 3) the decorator returns a new function as its return value.
+        if 'logged_in' in session: # check if logged_in key is in session dictionary.
+            return func()  # invoke the decorated func. The argument in check_logged_in func.
+        return 'You are NOT logged in'
+return wrapper # return the nested function as function object.
