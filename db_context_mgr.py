846dc664f561c4660ac406d34ce66916d2a836e0
mark-soro <napoleon_soro@rocketmail.com>
2020-06-23 15:38:18 -0400

    added CredentialsError and SQLError class

--- db_context_mgr.py
+++ db_context_mgr.py
@@ -3,13 +3,17 @@ import mysql.connector
 class ConnectionError(Exception):
 # create an exception handler class here in database context manager module,
 # instead of handling the exceptions for view_the_log code block itself, which means
-# needing to import the mysql connection and avoiding coupling of backend database.
+# needing to import the mysql connection and avoiding the coupling ofcodes to backend database.
 # we'll add try and except block to protect our __enter__ code block.
 # we're passing in the Exception which means all attributes and behaviors...
 # of exception is available to Connection Error.
-    pass
-    # no code needed here. pass.
+    pass # no code needed here. pass.
 
+class CredentialsError(Exception): # let's guard for wrong credentials.
+    pass                           # produces a ProgrammingError
+
+class SQLError(Exception): # let's guard for incorrect SQL commands
+    pass                   # produces a ProgrammingError
 
 class UseDatabase: # create class using class keyword and CamelCase style
 # to use the "with" statement we need to create a class with:
@@ -34,14 +38,28 @@ class UseDatabase: # create class using class keyword and CamelCase style
             # return the cursor
             return self.cursor
         except mysql.connector.errors.InterfaceError as err:
-        # catch the error.
-        # use the full name of the backend specific exception (mysql.connector.errors.InterfaceError).
+        # use the full name of the backend specific exception...
+        # mysql.connector.errors.InterfaceError occurs with database connection failure.
             raise ConnectionError(err)
             # raise the custom exception.
+        except mysql.connector.errors.ProgrammingError as err:
+        # mysql.connector.errors.ProgrammingError occurs w/ wrong credentials.
+            raise ConnectionError(err)
+            # raise the custom exception.
+
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         # annotation in method indicates this returns no value
-        # exception handlers are passed in as arguments.
+        # exception handlers are passed in as arguments -> exc_type, exc_value, exc_trace.
         # when something goes wrong the interpreter notifies the __exit__ .
         self.conn.commit() # commit to write the file in database
         self.cursor.close() # close the cursor
         self.conn.close() # close the connector
+        if exc_type is mysql.connector.ProgrammingError:
+        # let's put to work the exception handlers.
+        # exc_type for type of execptions and exc_value for exception's value.
+            raise SQLError(exc_value)
+            # raise the error.
+        elif exc_type:
+        # to catch any other exception thrown in at __exit__.
+            raise SQLError(exc_value)
+            # raise the error.      
